3594a3cae338ed3d5cfd71c624d6beb4ec675305
funnydevelopment <ya.giggs2013@yandex.ru>
2023-10-14 06:37:28 +0300

    added creating teachers url

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from core import services
 
 async def run_app():
     load_dotenv(".env")
-    await services.get_schools_urls()
+    #await services.get_schools_urls()
+    await services.get_teachers_urls()
 
 
 if __name__ == "__main__":
--- core/services.py
+++ core/services.py
@@ -1,6 +1,7 @@
 import os
 
 import httpx
+from bs4 import BeautifulSoup
 
 from core import database
 
@@ -30,3 +31,18 @@ async def get_teachers_urls():
     teachers_url = await database.get_json_data()
     for i in range(581):
         print(teachers_url[i])
+
+        #todo: вытащить ссылку из json, добавить вторую часть о нас и спарсить с помощью супа
+        url = 'https://sch1212sz.mskobr.ru/o-nas/pedagogicheskii-sostav#'
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+
+        if response.status_code == 200:
+            html_content = response.text
+            soup = BeautifulSoup(html_content, 'html.parser')
+
+            # Найти все элементы <a> с атрибутом href, содержащим "/teacher-card/"
+            teacher_links = soup.find_all('a', href=True, href="/teacher-card/")
+
+            for link in teacher_links:
+                print(link.get_text(strip=True))  # Вывести текст из элемента
