c7da459f19ef79e5b90b3e9b3b8317871ca22733
funnydevelopment <ya.giggs2013@yandex.ru>
2023-10-14 16:02:34 +0300

    changed an access to the json file

--- core/database.py
+++ core/database.py
@@ -1,20 +1,26 @@
 import json
+import os
+
+
+root_directory = os.getcwd()
+file_name = "data.json"
+file_path = os.path.join(root_directory, file_name)
 
 
 async def create_json_data(school_id: int, website: str) -> None:
     try:
-        with open("../data.json", "r", encoding="utf-8") as json_file:
+        with open(file_path, "r", encoding="utf-8") as json_file:
             data = json.load(json_file)
     except FileNotFoundError:
         data = []
 
     data.append({school_id: website})
 
-    with open("../data.json", "w", encoding="utf-8") as json_file:
+    with open(file_path, "w", encoding="utf-8") as json_file:
         json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 
 async def get_json_data() -> list:
-    with open("../data.json", "r", encoding="utf-8") as json_file:
+    with open(file_path, "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
     return data
