47845bf510bac179e8f4a31c01baeb336f3ecc33
funnydevelopment <ya.giggs2013@yandex.ru>
2023-10-14 19:50:33 +0300

    refactored json data 2 creator

--- core/services.py
+++ core/services.py
@@ -29,7 +29,7 @@ async def get_schools_urls():
 
 async def get_teachers_urls():
     schools_url = await database.get_json_data()
-    for i in range(558, 581):
+    for i in range(0, 582):
         school_url = schools_url[i][str(i + 1)]
 
         url = f"https://{school_url}/o-nas/pedagogicheskii-sostav#"
@@ -41,8 +41,16 @@ async def get_teachers_urls():
             soup = BeautifulSoup(html_content, "html.parser")
 
             teacher_links = soup.find_all("a", class_="fio")
-            teacher_url_list = [link.get("href") for link in teacher_links]
-            await database.create_json_data_2(i+1, teacher_url_list)
+            teacher_url_list = [
+                "https://" + school_url + link.get("href") for link in teacher_links
+            ]
+            await database.create_json_data_2(i + 1, teacher_url_list)
             print(f"Школа {i + 1}/581")
         else:
             print(f"Error:\n{response.status_code}")
+
+
+async def get_teachers_info():
+    schools_url = await database.get_json_data()
+    teachers_url = await database.get_json_data_2()
+    #todo: загрузить профиль сотрудника и через суп спарсить данные, потом сохранить
