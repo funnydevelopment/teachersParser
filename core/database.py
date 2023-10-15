bfc69d2f41b725ec60ee8da6f41e913ba2037666
funnydevelopment <ya.giggs2013@yandex.ru>
2023-10-15 15:52:13 +0300

    updated services

--- core/database.py
+++ core/database.py
@@ -3,10 +3,15 @@ import os
 
 
 root_directory = os.getcwd()
+
 file_name = "data.json"
 file_path = os.path.join(root_directory, file_name)
+
 file_name_2 = "data_2.json"
 file_2_path = os.path.join(root_directory, file_name_2)
+
+file_name_3 = "data_3.json"
+file_3_path = os.path.join(root_directory, file_name_3)
 
 
 async def create_json_data(school_id: int, website: str) -> None:
@@ -43,5 +48,24 @@ async def create_json_data_2(school_id: int, teacher_cards: list) -> None:
 
 async def get_json_data_2() -> list:
     with open(file_2_path, "r", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+async def create_json_data_3(incoming_data: dict) -> None:
+    try:
+        with open(file_3_path, "r", encoding="utf-8") as json_file:
+            data = json.load(json_file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = []
+
+    data.append(incoming_data)
+
+    with open(file_3_path, "w", encoding="utf-8") as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+
+async def get_json_data_3() -> list:
+    with open(file_3_path, "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
     return data
--- core/services.py
+++ core/services.py
@@ -1,4 +1,6 @@
+import asyncio
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 import httpx
 from bs4 import BeautifulSoup
@@ -50,7 +52,114 @@ async def get_teachers_urls():
             print(f"Error:\n{response.status_code}")
 
 
+# async def get_teachers_info():
+#     teachers_url = await database.get_json_data_2()
+#     for teacher_url in teachers_url:
+#         for key, value in teacher_url.items():
+#             urls_list = value
+#             for el in urls_list:
+#                 url = el
+#                 async with httpx.AsyncClient() as client:
+#                     response = await client.get(url)
+#
+#                 if response.status_code == 200:
+#                     html = response.text
+#                     soup = BeautifulSoup(html, "html.parser")
+#
+#                     teacher_div = soup.find("div", class_="panel_teacher groupteachers")
+#
+#                     title_spans = teacher_div.find_all("span", class_="title")
+#
+#                     kris_head_div = soup.find("div", class_="kris-component-head")
+#                     h1 = kris_head_div.find("h1")
+#                     fio = h1.text
+#
+#                     data_dict = dict()
+#
+#                     data_dict["ФИО"] = fio
+#                     data_dict["Ссылка"] = url
+#
+#                     for title_span in title_spans:
+#                         next_span = title_span.find_next_sibling("span")
+#                         if next_span:
+#                             title_text = title_span.text
+#                             next_text = next_span.text
+#                             for char in ["\xa0", "\n", "  "]:
+#                                 title_text = title_text.replace(char, "")
+#                                 next_text = next_text.replace(char, "")
+#
+#                             data_dict[title_text] = next_text
+#                     await database.create_json_data_3(data_dict)
+#                 else:
+#                     print(f"Error:\n{response.status_code}")
+
+
+async def process_url(url):
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+
+        if response.status_code == 200:
+            html = response.text
+            soup = BeautifulSoup(html, "html.parser")
+
+            teacher_div = soup.find("div", class_="panel_teacher groupteachers")
+
+            title_spans = teacher_div.find_all("span", class_="title")
+
+            kris_head_div = soup.find("div", class_="kris-component-head")
+            h1 = kris_head_div.find("h1")
+            fio = h1.text
+
+            data_dict = dict()
+
+            data_dict["ФИО"] = fio
+            data_dict["Ссылка"] = url
+
+            for title_span in title_spans:
+                next_span = title_span.find_next_sibling("span")
+                if next_span:
+                    title_text = title_span.text
+                    next_text = next_span.text
+                    for char in ["\xa0", "\n", "  "]:
+                        title_text = title_text.replace(char, "")
+                        next_text = next_text.replace(char, "")
+
+                    data_dict[title_text] = next_text
+            await database.create_json_data_3(data_dict)
+            print("Обработано 100 ссылок")
+        else:
+            print(f"Error for {url}:\n{response.status_code}")
+    except Exception as e:
+        print(f"Error for {url}:\n{e}")
+
+
+async def process_batch(urls):
+    loop = asyncio.get_running_loop()
+    with ThreadPoolExecutor() as executor:
+        await asyncio.gather(
+            *[
+                await loop.run_in_executor(executor, process_url, url)
+                for url in urls
+            ]
+        )
+
+
 async def get_teachers_info():
-    schools_url = await database.get_json_data()
     teachers_url = await database.get_json_data_2()
-    #todo: загрузить профиль сотрудника и через суп спарсить данные, потом сохранить
+    all_urls = []
+    for teacher_data in teachers_url:
+        for key, value in teacher_data.items():
+            if isinstance(value, list):
+                all_urls.extend(value)
+
+    # Разбиваем список URL-ов на пакеты, например, по 100 URL-ов в каждом
+    batch_size = 100
+    for i in range(0, len(all_urls), batch_size):
+        batch = all_urls[i:i + batch_size]
+        await process_batch(batch)
+
+
+async def get_len_json_data():
+    teachers = await database.get_json_data_3()
+    print(len(teachers))
